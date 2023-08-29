12a1880d133f462aa6db5f9f141d368c72aa3d23
ValeriyKryatov <Kwaler@mail.ru>
2023-08-29 23:18:54 +0300

    Homework2.12Testing. First commit

---
+++ src/main/java/HomeworkTesting/HomeworkTestingApplication.java
@@ -0,0 +1,13 @@
+package HomeworkTesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/HomeworkTesting/HomeworkTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package HomeworkTesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
